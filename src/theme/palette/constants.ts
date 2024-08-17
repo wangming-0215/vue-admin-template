35fcd6e874cf70458a86b4176c1b337eec049fa5
汪小明 <775899982@qq.com>
2024-08-17 22:56:32 +0800

    feat: refactor theme

--- src/theme/palette/constants.ts
+++
@@ -1,5 +0,0 @@
-type NaiveColorType = 'primary' | 'success' | 'error' | 'warning' | 'info';
-type NaiveColorScene = '' | 'Hover' | 'Pressed' | 'Suppl';
-
-export const types: NaiveColorType [] = ['primary', 'success', 'error', 'warning', 'info'];
-export const scenes: NaiveColorScene[] = ['', 'Hover', 'Pressed', 'Suppl'];
